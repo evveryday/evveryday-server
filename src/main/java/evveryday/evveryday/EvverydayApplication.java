a034bd82caba9e9d136e9bb643d13a5c8790f4dd
seohyejun <shyejun0718@naver.com>
2023-08-17 21:57:52 +0900

    Init: 추가 초기 세팅

---
+++ src/main/java/evveryday/evveryday/EvverydayApplication.java
@@ -0,0 +1,13 @@
+package evveryday.evveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvverydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvverydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evveryday/evveryday/EvverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package evveryday.evveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvverydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
