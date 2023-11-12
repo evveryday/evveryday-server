2d928ccd31687afba9bc553efa85fd6c742ca839
seohyejun <shyejun0718@naver.com>
2023-11-12 12:58:28 +0900

    Fix: 삭제 테스트

--- src/test/java/evveryday/evveryday/EvverydayApplicationTests.java
+++ src/test/java/evveryday/evveryday/EvverydayApplicationTests.java
@@ -3,11 +3,11 @@ package evveryday.evveryday;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class EvverydayApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
