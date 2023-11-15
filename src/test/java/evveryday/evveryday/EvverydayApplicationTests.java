aa16e835630aca16ed507638e3711dbdaab7af2f
heewonsuhh <shw981110@naver.com>
2023-11-16 03:50:42 +0900

    Fix: 삭제 테스트

--- src/test/java/evveryday/evveryday/EvverydayApplicationTests.java
+++ src/test/java/evveryday/evveryday/EvverydayApplicationTests.java
@@ -1,4 +1,4 @@
-package evveryday.evveryday;
+/*package evveryday.evveryday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class EvverydayApplicationTests {
 	}
 
 }
+*/
